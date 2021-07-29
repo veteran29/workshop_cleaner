7904a2beb4b008a460de105ec6f4a9d8a180d5e3
Filip Maciejewski <veteran29@users.noreply.github.com>
2021-07-29 22:56:51 +0200

    Add ToAppId trait to PathBuf

--- workshop_cleaner-core/src/lib.rs
+++ workshop_cleaner-core/src/lib.rs
@@ -1,12 +1,13 @@
 use std::thread;
 
-use steamworks::Client;
+use steamworks::{AppIDs, AppId, Client};
 
 pub use steamworks::{AppId, PublishedFileId};
 
 use crate::cleaner::WorkshopCleaner;
 
 pub mod cleaner;
+pub mod locator;
 
 pub fn init(app_id: AppId) -> Result<WorkshopCleaner, String> {
     std::fs::write("steam_appid.txt", format!("{}", app_id.0))
@@ -33,6 +34,22 @@ impl ToPublishedFileId for std::path::PathBuf {
             None => None,
             Some(file_name) => match file_name.to_str().unwrap().parse::<u64>() {
                 Ok(item_id) => Some(PublishedFileId(item_id)),
+                Err(_) => None,
+            },
+        }
+    }
+}
+
+trait ToAppId {
+    fn to_app_id(&self) -> Option<AppId>;
+}
+
+impl ToAppId for std::path::PathBuf {
+    fn to_app_id(&self) -> Option<AppId> {
+        match self.file_name() {
+            None => None,
+            Some(file_name) => match file_name.to_str().unwrap().parse::<u32>() {
+                Ok(item_id) => Some(AppId(item_id)),
                 Err(_) => None,
             },
         }
