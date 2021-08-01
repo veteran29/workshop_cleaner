a4b72ca09325e27fd28d25d227f90397a0b63f8f
Filip Maciejewski <veteran29@users.noreply.github.com>
2021-08-01 17:49:22 +0200

    Code format

--- workshop_cleaner-gui/src/main.rs
+++ workshop_cleaner-gui/src/main.rs
@@ -1,10 +1,7 @@
 use druid::im::{vector, Vector};
 use druid::lens::{self, LensExt};
 use druid::widget::{Button, Flex, Label, List, Scroll, Split};
-use druid::{
-    AppLauncher, Color, Data, Lens, UnitPoint, Widget, WidgetExt, WindowDesc,
-};
-
+use druid::{AppLauncher, Color, Data, Lens, UnitPoint, Widget, WidgetExt, WindowDesc};
 
 #[derive(Clone, Data, Lens)]
 struct AppState {
@@ -15,8 +12,7 @@ struct AppState {
 fn main() {
     let main_window = WindowDesc::new(ui_builder)
         .window_size((1000.0, 512.0))
-        .title("Workshop Cleaner")
-    ;
+        .title("Workshop Cleaner");
 
     let state = AppState {
         apps: vector!("Arma 3".to_string(), "Dota 2".to_string()),
@@ -26,8 +22,7 @@ fn main() {
     AppLauncher::with_window(main_window)
         .use_simple_logger()
         .launch(state)
-        .expect("App launch failed")
-    ;
+        .expect("App launch failed");
 }
 
 const MAIN_LAYOUT_SPLIT_POINT: f64 = 0.3;
@@ -35,13 +30,11 @@ const MAIN_LAYOUT_SPLIT_POINT: f64 = 0.3;
 fn ui_builder() -> impl Widget<AppState> {
     let left_col = Scroll::new(List::new(app_widget))
         .vertical()
-        .lens(AppState::apps)
-    ;
+        .lens(AppState::apps);
 
     let right_col = Scroll::new(List::new(item_widget))
-    .vertical()
-    .lens(AppState::items)
-;
+        .vertical()
+        .lens(AppState::items);
 
     Split::columns(left_col, right_col)
         .split_point(MAIN_LAYOUT_SPLIT_POINT)
@@ -54,22 +47,18 @@ const LIST_ITEM_HEIGHT: f64 = 50.0;
 const LIST_ITEM_PADDING: f64 = 10.0;
 
 fn app_widget() -> impl Widget<String> {
-    Label::new(|item: &String, _env: &_| {
-        format!("App: {}", item)
-    })
-    .align_vertical(UnitPoint::LEFT)
-    .padding(LIST_ITEM_PADDING)
-    .expand()
-    .height(LIST_ITEM_HEIGHT)
+    Label::new(|item: &String, _env: &_| format!("App: {}", item))
+        .align_vertical(UnitPoint::LEFT)
+        .padding(LIST_ITEM_PADDING)
+        .expand()
+        .height(LIST_ITEM_HEIGHT)
 }
 
 fn item_widget() -> impl Widget<String> {
     Flex::row()
         .with_child(
-            Label::new(|item: &String, _env: &_| {
-                format!("List item #{}", item)
-            })
-            .align_vertical(UnitPoint::LEFT),
+            Label::new(|item: &String, _env: &_| format!("List item #{}", item))
+                .align_vertical(UnitPoint::LEFT),
         )
         .with_flex_spacer(1.0)
         .with_child(
