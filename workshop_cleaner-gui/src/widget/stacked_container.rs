2406105b03aa0f4789a16b563e2b74bc0cb2b4be
Filip Maciejewski <veteran29@users.noreply.github.com>
2021-09-03 02:23:45 +0200

    Add top item visiblity and passthrough callbacks to StackedContainer

--- workshop_cleaner-gui/src/widget/stacked_container.rs
+++ workshop_cleaner-gui/src/widget/stacked_container.rs
@@ -1,16 +1,50 @@
 use druid::{Data, Point, Size, Widget, WidgetPod};
 
+type ShowOverlayCallback<T> = dyn Fn(&T) -> bool;
+type PassthroughOverlayEventsCallback<T> = dyn Fn(&T) -> bool;
+
+/// A widget that allows rendering of two widgets on top of each other.
 pub struct StackedContainer<T> {
     inner_bottom: WidgetPod<T, Box<dyn Widget<T>>>,
     inner_top: WidgetPod<T, Box<dyn Widget<T>>>,
+    show_overlay_callback: Box<ShowOverlayCallback<T>>,
+    passthrough_overlay_events_callback: Box<PassthroughOverlayEventsCallback<T>>,
+    overlay_shown: bool,
 }
 
 impl<T: Data> StackedContainer<T> {
+    /// Create stacked container with two child widgets.
     pub fn new(bottom: impl Widget<T> + 'static, top: impl Widget<T> + 'static) -> Self {
         Self {
             inner_bottom: WidgetPod::new(bottom).boxed(),
             inner_top: WidgetPod::new(top).boxed(),
+            show_overlay_callback: Box::new(|_| false),
+            passthrough_overlay_events_callback: Box::new(|_| false),
+            overlay_shown: false,
         }
+    }
+
+    /// Builder style method for setting show overlay callback.
+    ///
+    /// Visibility of the top element depends on the return value of this callback.
+    /// No events will be sent to top element if it's not visible.
+    pub fn with_show_overlay_callback(mut self, callback: Box<ShowOverlayCallback<T>>) -> Self {
+        self.show_overlay_callback = callback;
+
+        self
+    }
+
+    /// Builder style method for setting passthrough overlay events callback.
+    ///
+    /// When the callback returns `true` then events will be passed to the bottom widget,
+    /// in case `false` will be returned then events will "stop" at the top element.
+    pub fn with_passthrough_overlay_events_callback(
+        mut self,
+        callback: Box<ShowOverlayCallback<T>>,
+    ) -> Self {
+        self.passthrough_overlay_events_callback = callback;
+
+        self
     }
 }
 
@@ -23,8 +57,13 @@ impl<T: Data> Widget<T> for StackedContainer<T> {
         data: &mut T,
         env: &druid::Env,
     ) {
-        self.inner_bottom.event(ctx, event, data, env);
-        self.inner_top.event(ctx, event, data, env);
+        self.overlay_shown = (self.show_overlay_callback)(data);
+        if !self.overlay_shown || (self.passthrough_overlay_events_callback)(data) {
+            self.inner_bottom.event(ctx, event, data, env);
+        }
+        if self.overlay_shown {
+            self.inner_top.event(ctx, event, data, env);
+        }
     }
 
     fn lifecycle(
@@ -64,6 +103,8 @@ impl<T: Data> Widget<T> for StackedContainer<T> {
 
     fn paint(&mut self, ctx: &mut druid::PaintCtx, data: &T, env: &druid::Env) {
         self.inner_bottom.paint(ctx, data, env);
-        self.inner_top.paint(ctx, data, env);
+        if self.overlay_shown {
+            self.inner_top.paint(ctx, data, env);
+        }
     }
 }
